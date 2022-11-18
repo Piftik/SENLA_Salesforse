55b745147a4d8ed2760626234baf4eb4cf874cdc
Piftik <konstantin-poroh@mail.ru>
2022-11-18 11:52:10 +0300

    Fix Homework_FE_2 and add Utils

--- VSCodeQuickstart/force-app/main/default/lwc/contactCard/contactCard.js
+++ VSCodeQuickstart/force-app/main/default/lwc/contactCard/contactCard.js
@@ -2,15 +2,14 @@ import { LightningElement, api, wire } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
 import { getRecord, getFieldValue } from "lightning/uiRecordApi";
 
-import FIRST_NAME_FIELD from "@salesforce/schema/Prorerty_New__c.Property_Owner__r.FirstName";
-import LAST_NAME_FIELD from "@salesforce/schema/Prorerty_New__c.Property_Owner__r.LastName";
-import PHONE_FIELD from "@salesforce/schema/Prorerty_New__c.Property_Owner__r.Phone";
-import HOME_PHONE_FIELD from "@salesforce/schema/Prorerty_New__c.Property_Owner__r.HomePhone";
+import FIRST_NAME_FIELD from "@salesforce/schema/Property_New__c.Property_Owner__r.FirstName";
+import LAST_NAME_FIELD from "@salesforce/schema/Property_New__c.Property_Owner__r.LastName";
+import PHONE_FIELD from "@salesforce/schema/Property_New__c.Property_Owner__r.Phone";
+import HOME_PHONE_FIELD from "@salesforce/schema/Property_New__c.Property_Owner__r.HomePhone";
 import EMAIL_FIELD from "@salesforce/schema/Property_New__c.Property_Owner__r.Email";
-import TOTAL_PRICE_FIELD from "@salesforce/schema/Prorerty_New__c.Property_Owner__r.Total_Property_price__c";
-
-import PROPERTY_ID from "@salesforce/schema/Property_New__c.Id";
-import PROPERTY_OWNER from "@salesforce/schema/Property_New__c.Property_Owner__r.Id";
+import TOTAL_PRICE_FIELD from "@salesforce/schema/Property_New__c.Property_Owner__r.Total_Property_price__c";
+import PROPERTY_OWNER from "@salesforce/schema/Property_New__c.Property_Owner__c";
+import PROPERTY from "@salesforce/schema/Property_New__c.Property_Owner__r.id";
 
 const fields = [
   FIRST_NAME_FIELD,
@@ -18,17 +17,15 @@ const fields = [
   PHONE_FIELD,
   HOME_PHONE_FIELD,
   EMAIL_FIELD,
-  TOTAL_PRICE_FIELD
+  TOTAL_PRICE_FIELD,
+  PROPERTY_OWNER,
+  PROPERTY
 ];
 
-export default class ProrertyOwnerCard extends LightningElement {
+export default class ProrertyOwnerCard extends NavigationMixin(
+  LightningElement
+) {
   @api recordId;
-
-  @wire(getRecord, {
-    recordId: "$recordId",
-    fields: [PROPERTY_ID, PROPERTY_OWNER]
-  })
-  property;
 
   @wire(getRecord, { recordId: "$recordId", fields })
   owner;
@@ -54,16 +51,15 @@ export default class ProrertyOwnerCard extends LightningElement {
   get total() {
     return getFieldValue(this.owner.data, TOTAL_PRICE_FIELD);
   }
-
-  get propertyOwnerId() {
-    return getFieldValue(this.property.data, PROPERTY_OWNER);
+  get ownerContact() {
+    return getFieldValue(this.owner.data, PROPERTY_OWNER);
   }
 
   navigateToViewContactPage() {
     this[NavigationMixin.Navigate]({
       type: "standard__recordPage",
       attributes: {
-        recordId: this.ownerId,
+        recordId: this.ownerContact,
         objectApiName: "Contact",
         actionName: "view"
       }
--- VSCodeQuickstart/force-app/main/default/lwc/personsInformation/personsInformation.js
+++ VSCodeQuickstart/force-app/main/default/lwc/personsInformation/personsInformation.js
@@ -1,194 +1,74 @@
-import { LightningElement, api } from "lwc";
-
-const persons = [
-  {
-    id: 1,
-    firstName: "Olga",
-    lastName: "Ivanova",
-    gender: "F",
-    birthday: new Date(2000, 1, 1),
-    email: "ivanova@salesforce.com"
-  },
-  {
-    id: 2,
-    firstName: "Slava",
-    lastName: "Petrov",
-    gender: "M",
-    birthday: new Date(2002, 2, 2),
-    email: "petrov@salesforce.com"
-  },
-  {
-    id: 3,
-    firstName: "Petr",
-    lastName: "Sidorov",
-    gender: "M",
-    birthday: new Date(2003, 3, 3),
-    email: "sidorov@salesforce.com"
-  },
-  {
-    id: 4,
-    firstName: "Darya",
-    lastName: "Zdarova",
-    gender: "F",
-    birthday: new Date(2004, 4, 4),
-    email: "zdarova@salesforce.com"
-  },
-  {
-    id: 5,
-    firstName: "Kostia",
-    lastName: "Frolov",
-    gender: "M",
-    birthday: new Date(2000, 5, 5),
-    email: "frolov@salesforce.com"
-  },
-  {
-    id: 6,
-    firstName: "Eva",
-    lastName: "Koroleva",
-    gender: "F",
-    birthday: new Date(2001, 6, 6),
-    email: "koroleva@salesforce.com"
-  },
-  {
-    id: 7,
-    firstName: "Tania",
-    lastName: "Boldina",
-    gender: "F",
-    birthday: new Date(2002, 7, 7),
-    email: "boldina@salesforce.com"
-  },
-  {
-    id: 8,
-    firstName: "Katia",
-    lastName: "Sorokina",
-    gender: "F",
-    birthday: new Date(2003, 8, 8),
-    email: "sorokina@salesforce.com"
-  },
-  {
-    id: 9,
-    firstName: "Igor",
-    lastName: "Seilov",
-    gender: "M",
-    birthday: new Date(2000, 9, 9),
-    email: "seilov@salesforce.com"
-  },
-  {
-    id: 10,
-    firstName: "Liuda",
-    lastName: "Kaluta",
-    gender: "F",
-    birthday: new Date(2001, 10, 10),
-    email: "kaluta@salesforce.com"
-  }
-];
-const columns = [
-  { label: "Firstname", fieldName: "firstName" },
-  { label: "LastName", fieldName: "lastName" },
-  { label: "Gender", fieldName: "gender" },
-  { label: "Birthday", fieldName: "birthday", type: "date" },
-  { label: "Email", fieldName: "email", type: "email" }
-];
-const options = [
-  { value: "firstName", label: "FirstName" },
-  { value: "lastName", label: "LastName" },
-  { value: "email", label: "Email" }
-];
+import { LightningElement } from "lwc";
+import { persons, columns, options } from "c/utils";
 
 export default class PersonsInformation extends LightningElement {
-  data = [...persons];
+  data = persons;
   columns = columns;
-  options = options;
   value = "sortBy";
-  filterGender = "";
-  dateRange = ["", ""];
-  @api
-  handleChange(event) {
+  options = options;
+
+  optionsGender = [
+    { label: "Male", value: "Male", checked: null },
+    { label: "Female", value: "Female", checked: null }
+  ];
+
+  handleSortBy(event) {
     this.value = event.detail.value;
-    const result = JSON.parse(JSON.stringify(this.persons)).sort((a, b) =>
+    const resultSort = JSON.parse(JSON.stringify(this.data)).sort((a, b) =>
       a[event.detail.value].localeCompare(b[event.detail.value])
     );
-    this.data = result;
+    this.data = resultSort;
   }
-  @api
-  handleChangeFrom(event) {
+
+  handleChangeDateFrom(event) {
     const choosedDate = new Date(event.detail.value);
-    const result = this.persons.filter((item) => {
+    const resultDateFilter = this.data.filter((item) => {
       return new Date(item.birthday) >= choosedDate;
     });
-    this.persons = result;
+    this.data = resultDateFilter;
   }
-  @api
-  handleChangeTo(event) {
+
+  handleChangeDateTo(event) {
     const choosedDate = new Date(event.detail.value);
-    const result = this.data.filter((item) => {
+    const resultDateToFilter = this.data.filter((item) => {
       return new Date(item.birthday) <= choosedDate;
     });
-    this.data = result;
+    this.data = resultDateToFilter;
   }
-  @api
-  handleGenderFilter(event) {
-    this.filterGender = "";
-    Array.from(this.template.querySelectorAll('[data-id="checkbox"]')).forEach(
-      (element) => {
-        if (element === event.target && element.checked) {
-          this.filterGender = element.name;
-        } else element.checked = false;
-      }
-    );
-    let records = [];
-    if (this.filterGender !== "") {
-      for (let record of this.data) {
-        if (record.gender === this.filterGender) {
-          records.push(record);
-        }
-      }
-      this.data = records;
-    } else this.checkGenders();
+
+  handleChangeGender(e) {
+    const valueGender = e.target.name;
+    if (valueGender === "Male") {
+      this.optionsGender[1].checked = 0;
+      this.optionsGender[0].checked = 1;
+    } else {
+      this.optionsGender[0].checked = 0;
+      this.optionsGender[1].checked = 1;
+    }
+    const resultFilterByGender = persons.filter((item) => {
+      return item.gender === valueGender;
+    });
+    this.data = resultFilterByGender;
   }
-  @api
+
   handleChangeEmail(event) {
-    const result = persons.filter((item) => {
+    const resultFilterByEmail = persons.filter((item) => {
       return item.email
         .toLowerCase()
         .includes(event.target.value.toLowerCase());
     });
-    this.data = result;
+    this.data = resultFilterByEmail;
   }
-  @api
+
   handleReset() {
-    this.data = [...persons];
-    this.filterGender = "";
-    this.dateRange = ["", ""];
     this.value = "";
-
-    this.template.querySelector('[data-id="sortByBox"]').value = "";
-    this.template.querySelector('[data-id="fromSort"]').value = "";
-    this.template.querySelector('[data-id="toSort"]').value = "";
-    this.template.querySelector('[data-id="emailInput"]').value = "";
-    Array.from(
-      this.template.querySelectorAll('[data-id = "checkbox"]')
-    ).forEach((element) => {
-      element.checked = false;
+    this.template.querySelectorAll("lightning-input").forEach((element) => {
+      if (element.type === "checkbox") {
+        element.checked = false;
+      } else {
+        element.value = null;
+      }
+      this.data = persons;
     });
-  }
-
-  checkGenders() {
-    const male = this.data.find((person) => person.gender === "M");
-    const female = this.data.find((person) => person.gender === "F");
-    let checkboxes = Array.from(
-      this.template.querySelectorAll('[data-id="checkbox"]')
-    );
-    checkboxes.forEach((element) => (element.disabled = false));
-
-    if (male === undefined) {
-      let element = checkboxes.find((element) => element.name === "M");
-      element.disabled = true;
-    }
-
-    if (female === undefined) {
-      let element = checkboxes.find((element) => element.name === "F");
-      element.disabled = true;
-    }
   }
 }
