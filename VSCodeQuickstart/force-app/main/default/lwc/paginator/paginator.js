81c890af5af4f2d0e1ac8593597d64b68bc77071
Piftik <konstantin-poroh@mail.ru>
2022-11-28 11:11:14 +0300

    Fix HomeWork_FE_5

--- VSCodeQuickstart/force-app/main/default/lwc/paginator/paginator.js
+++ VSCodeQuickstart/force-app/main/default/lwc/paginator/paginator.js
@@ -2,9 +2,36 @@ import { LightningElement, api } from "lwc";
 
 export default class Pagination extends LightningElement {
   currentPage = 1;
-
-  @api
+  totalRecords;
+  visibleRecords;
+  @api recordsSize = 8;
   totalPages = 0;
+
+  get records() {
+    return this.visibleRecords;
+  }
+  @api
+  set records(data) {
+    if (data) {
+      this.totalRecords = data;
+      this.recordSize = Number(this.recordSize);
+      this.totalPage = Math.ceil(data.length / this.recordSize);
+      this.updateRecords();
+    }
+  }
+
+  updateRecords() {
+    const start = (this.currentPage - 1) * this.recordSize;
+    const end = this.recordSize * this.currentPage;
+    this.visibleRecords = this.totalRecords.slice(start, end);
+    this.dispatchEvent(
+      new CustomEvent("update", {
+        detail: {
+          records: this.visibleRecords
+        }
+      })
+    );
+  }
 
   get disablePrevious() {
     return this.currentPage <= 1;
@@ -16,19 +43,14 @@ export default class Pagination extends LightningElement {
   previousHandler() {
     if (this.currentPage > 1) {
       this.currentPage = this.currentPage - 1;
-      this.dispatchEvent(
-        new CustomEvent("updatepagerecords", { detail: this.currentPage })
-      );
+      this.updateRecords();
     }
   }
 
   nextHandler() {
     if (this.currentPage < this.totalPage) {
-      console.log("total page paginator -" + this.totalPage);
       this.currentPage += 1;
-      this.dispatchEvent(
-        new CustomEvent("updatepagerecords", { detail: this.currentPage })
-      );
+      this.updateRecords();
     }
   }
 }
