e86a64d4293bc6ecf05f63e2ed5bab327a230342
Piftik <konstantin-poroh@mail.ru>
2022-12-01 03:32:15 +0300

    HomeWork_FE_6

--- VSCodeQuickstart/force-app/main/default/lwc/paginator/paginator.js
+++ VSCodeQuickstart/force-app/main/default/lwc/paginator/paginator.js
@@ -1,37 +1,8 @@
 import { LightningElement, api } from "lwc";
 
 export default class Pagination extends LightningElement {
+  @api totalPages = 0;
   currentPage = 1;
-  totalRecords;
-  visibleRecords;
-  @api recordsSize = 8;
-  totalPages = 0;
-
-  get records() {
-    return this.visibleRecords;
-  }
-  @api
-  set records(data) {
-    if (data) {
-      this.totalRecords = data;
-      this.recordSize = Number(this.recordSize);
-      this.totalPage = Math.ceil(data.length / this.recordSize);
-      this.updateRecords();
-    }
-  }
-
-  updateRecords() {
-    const start = (this.currentPage - 1) * this.recordSize;
-    const end = this.recordSize * this.currentPage;
-    this.visibleRecords = this.totalRecords.slice(start, end);
-    this.dispatchEvent(
-      new CustomEvent("update", {
-        detail: {
-          records: this.visibleRecords
-        }
-      })
-    );
-  }
 
   get disablePrevious() {
     return this.currentPage <= 1;
@@ -43,14 +14,18 @@ export default class Pagination extends LightningElement {
   previousHandler() {
     if (this.currentPage > 1) {
       this.currentPage = this.currentPage - 1;
-      this.updateRecords();
+      this.dispatchEvent(
+        new CustomEvent("updaterecords", { detail: this.currentPage })
+      );
     }
   }
 
   nextHandler() {
     if (this.currentPage < this.totalPage) {
       this.currentPage += 1;
-      this.updateRecords();
+      this.dispatchEvent(
+        new CustomEvent("updaterecords", { detail: this.currentPage })
+      );
     }
   }
 }
--- VSCodeQuickstart/force-app/main/default/lwc/propertyList/propertyList.js
+++ VSCodeQuickstart/force-app/main/default/lwc/propertyList/propertyList.js
@@ -1,40 +1,57 @@
 import { LightningElement, wire } from "lwc";
 import getPropertyList from "@salesforce/apex/PropertyController.getPropertyList";
-const Propertyfields = "Adress__c,Cost_Sale__c,Owner__r.Name,Name";
+import { PROPERTY_FIELDS } from "c/utils";
+
 export default class PropertyList extends LightningElement {
   totalProperties;
   visibleProperty;
   error;
+  spinner = true;
+  isError = false;
   showDetails = false;
-  selectedProp;
+  selectedProperty;
+  totalPages;
+  currentPage = 1;
+  recordsPage = 8;
 
-  @wire(getPropertyList, { fields: Propertyfields })
+  @wire(getPropertyList, { fields: PROPERTY_FIELDS.join(", ") })
   wiredProperties({ error, data }) {
     if (data) {
       this.totalProperties = data;
-      console.log(
-        "total properties ---" + JSON.stringify(this.totalProperties)
+      this.totalPages = Math.ceil(
+        this.totalProperties.length / this.recordsPage
       );
+      this.visibleProperty = this.totalProperties.slice(0, this.recordsPage);
+      this.error = undefined;
+      this.spinner = false;
     }
     if (error) {
-      this.error = error;
+      this.error = {
+        header: "Get properties error",
+        message: error.message
+      };
+      this.isError = true;
+
       console.error(error);
     }
   }
+
   updatePropertyHandler(event) {
-    console.log("Records from child--" + event.detail.records);
-    this.visibleProperty = [...event.detail.records];
+    this.currentPage = event.detail;
+    const start = (this.currentPage - 1) * this.recordsPage;
+    const end = this.recordsPage * this.currentPage;
+    this.visibleProperty = this.totalProperties.slice(start, end);
   }
 
   openDetails(event) {
-    let selectedPropId = event.detail;
-    this.selectedProp = this.totalProperties.find(
-      (property) => property.Id === selectedPropId
-    );
+    console.log(event.detail);
+    this.selectedProperty = event.detail;
+    this.spinner = true;
     this.showDetails = true;
   }
 
   hideDetails() {
     this.showDetails = false;
+    this.spinner = true;
   }
 }
